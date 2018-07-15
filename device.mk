8b0642ad6bc1666f190d777d0c131c6255ec81a5
Christian Oder <myself5@carbonrom.org>
2018-07-15 21:31:07 +0200

    davinci: Add basic permission file for HotwordEnrollment
    Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

--- device.mk
+++ device.mk
@@ -223,6 +223,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# Hotword Enrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom_ramdisk \
