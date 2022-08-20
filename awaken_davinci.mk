90e0690727e4678f5595278b5613b4bfcedcd392
ad1tyas1ngh <adityasingh1768@gmail.com>
2022-08-20 14:47:58 +0530

    davinci: Build gapps package and add maintainer overlay
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- awaken_davinci.mk
+++ awaken_davinci.mk
@@ -15,6 +15,15 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 $(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Extras
+AWAKEN_BUILD_TYPE := OFFICIAL
+USE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+
 PRODUCT_NAME := awaken_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
